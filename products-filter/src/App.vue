b9d853ae8c2c5d6f0dfd764f10a6abcfd32582eb
jokerharlui1782-eng <jokerharlui1782@gmail.com>
2025-10-26 20:36:06 +0300

    aba

---
+++ products-filter/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    
+    <h1>Ежедневные задания </h1>
+
+    <div class="products">
+      <TaskItem 
+        v-for="task in tasks" 
+        :key="task._id"  
+        :task="task"
+       >
+        
+    </TaskItem>
+    </div>
+
+  
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import TaskItem from "./components/TaskItem.vue";
+
+const tasks = [
+  { _id: "1", title: "Завтрак", text: "Приготовить завтрак и поесть" },
+  { _id: "2", title: "пробежка", text: "после завтракак пойти на пробежку" },
+  { _id: "3", title: "учёба", text: "пойти на пары" },
+  { _id: "4", title: "пойти на работу", text: "после пар идити на работу" },
+  { _id: "5", title: "Ужин", text: "Приготовить ужин и поесть" },
+  { _id: "6", title: "спать", text: "Приготовиться ко сну" },
+];
+
+
+
+</script>
+
+<style>
+.products {
+  display: flex;
+  gap: 13px;
+  flex-direction: column;
+}
+</style>
+
---
+++ products-filter/src/components/TaskItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="task-card">
+    <header>
+      <div><b>{{ props.task.title }}</b></div>
+    </header>
+
+    <main>
+      <div> Задание: {{ props.task.text }} </div>
+      <slot></slot>
+    </main>
+
+    <footer>
+      <button type="button" @click="complete">выполнить</button>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  task: { title: string; text: string};
+}>();
+
+function complete() {
+  alert(`ура,ты выполнил задание ${props.task.title}`);
+}
+</script>
+
+<style>
+.task-card {
+  display: flex;
+  gap: 4px;
+  flex-direction: column;
+  border: 1px solid #ccc;
+  padding: 12px;
+  border-radius: 8px;
+}
+</style>
+
